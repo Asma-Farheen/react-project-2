99389cbdd46eaaf8faf503629281f84a317ffa8f
Asma-Farheen <asmafarheen2025@gmail.com>
2025-11-13 12:47:01 +0530

    Initial commit for new Movie App Frontend repository

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "3a7f34e086d809d34cd6f95c336c5eb7";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
